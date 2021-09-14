015c75617b4591693835c0de23e4840f6a338713
Mahesh Babu <crazymahesh98@gmail.com>
2021-09-14 09:03:47 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/flightbookingsystem/FlightBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.flightbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/flightbookingsystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
